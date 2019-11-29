f2c4b1563edaf6a2371726f6d5850099de2a1f8e
Shen Satkunarasa <shensatkunarasa@Shen-Satkunarasas-MacBook-Pro.local>
2019-11-29 19:53:19 +0000

    Add test for rendering page, add gem for using assert_template method

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  
+  
   test "should get landing_page" do
     get pages_landing_page_url
+    
     assert_response :success
-  end
-
-  test "should have a image" do
-    assert_select, 
+    assert_template :landing_page
   end
 
 end
